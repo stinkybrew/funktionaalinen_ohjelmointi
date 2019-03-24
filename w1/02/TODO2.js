c674e02d314c5f4a9b66c38009a98d78e2d8e913
stinkybrew <wille.tuovinen@metropolia.fi>
2019-03-24 23:15:23 +0200

    Update TODO2.js
    ...

--- w1/02/TODO2.js
+++ w1/02/TODO2.js
@@ -1,63 +1,46 @@
 'use strict'
 
-const func = function (x, y) {
+const func = function(x, y) {
     return function () {
         if (x > y){
-            console.log(x+" is bigger that "+y);
+            //console.log(x+" is bigger that "+y);
             return (1); 
         }
         else if (x < y) {
-            console.log(x+" is smaller that "+y);
+            //console.log(x+" is smaller that "+y);
             return(-1);
         }
         else {
-            console.log(y+" and "+x+" are equal");
             return (0);
         }   
     }();
 }
+const f = function( func, v15, v16){
+    if (tulos === -1) {
+        console.log(v15+" is colder that "+v16);
+    }
+    else if (tulos === 1) {
+        console.log(v15+" is warmer that "+v16);
+    }
+    else  {
+        console.log(v15+" and "+v16+" temperaters are the same" );
+    }
+    // perussilmukka
 
-
+    //var tulos = f(func(v15, v16));
+    //console.log(tulos); 
+    return (v15, v16);
+    //return func(v2015[i]+1,v2015[i]+1);
+}
 var v2015 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 var v2016 = [2, 3, 3, 2, 4, 5, 7, 9, 8, 10, 10, 13];
 for(var i=0 ; i<v2015.length ;) {
     var v15 = v2015[i];
     var v16 = v2016[i];
-    var tulos = func(v15, v16);
-    console.log(tulos);
+    //console.log(tulos);
     i++
-}
-
-function f(v2015, v2016, tulos, func){
-    if (tulos === -1) {
-        
-    }
-    if (v2015[11]) {
-        return (true);
-    }
-    else if (v2015[i] < v2016[i]) {
-        return (false);
-    }
-    // perussilmukka
+    var tulos = (func(v15, v16));
+    var tlz = (f(tulos, v15, v16));
     console.log(tulos);
-    
-    return func(v2015[i]+1,v2015[i]+1);
+    //console.log(tlz); 
 }
-
-
-/*
-//////////////////////////////////////////////////
-function tervehdysteksti(nimi){
-    return 'Moi, '+ nimi;
-}
-function anteeksipyynto(nimi){
-    return 'Anteeksi, '+ nimi;
-}
-function annaFraasi(fraasi,nimi) { 
-   return fraasi(nimi); // paluuarvona fraasi-funktion kutsun tulos
-}
-console.log(annaFraasi(tervehdysteksti, 'Masa')); 
-console.log(annaFraasi(anteeksipyynto, 'Aku')); 
-
-// Jos fraasi, esim. tervehdys, muuttuu,
-// muutetaan vain yhteen paikkaan. Kutsut eivät muutu. */
