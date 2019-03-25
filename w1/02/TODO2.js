3e22440e25c665f08bef25eb56aca3a80fbf72d2
Wille Tuovinen <wille.tuovinen@metropolia.fi>
2019-03-25 11:45:57 +0200

    ..
    ...

--- w1/02/TODO2.js
+++ w1/02/TODO2.js
@@ -13,30 +13,36 @@ const func = function(x, y) {
         }   
     }();
 }
-const f = function(tulos, v15, v16){
+const f = function(list ,tulos, v15, v16){
+    
     if (tulos === -1) {
         console.log(v15+" is colder than "+v16);
+        list[0]+tulos;
     }
     else if (tulos === 1) {
         console.log(v15+" is warmer than "+v16);
+        list[1]+tulos;
     }
     else  {
         console.log(v15+" and "+v16+" temperatures are the same" );
+        list[2]+tulos;
     }
-    return (v15, v16);
-    //var tulos = f(func(v15, v16));
-    //console.log(tulos); 
-    //return func(v2015[i]+1,v2015[i]+1);
+    return (list, v15, v16);
+    // var tulos = f(func(v15, v16));
+    // console.log(tulos); 
+    // return func(v2015[i]+1,v2015[i]+1);
 }
 var v2015 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 var v2016 = [2, 3, 3, 2, 4, 5, 7, 9, 8, 10, 10, 13];
 for(var i=0 ; i<v2015.length ;) {
     var v15 = v2015[i];
     var v16 = v2016[i];
-    //console.log(tulos);
+    // console.log(tulos);
     i++
+    var list=[];
     var tulos = (func(v15, v16));
-    var tlz = (f(tulos, v15, v16));
+    var tlz = (f(list ,tulos, v15, v16));
     console.log(tulos);
-    //console.log(tlz); 
+    console.timeLog(list);
+    // console.log(tlz); 
 }
