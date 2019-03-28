96729a19a134b33d236a5f0886af2a7f026d7dab
Wille Tuovinen <wille.tuovinen@metropolia.fi>
2019-03-28 13:49:16 +0200

    ....
    ....

--- w2/01/TODO2.js
+++
@@ -1,8 +0,0 @@
-
-
-require(['movies.js'], function(source) {
-    console.log(source);
-    //Your script goes here
-    //some-dependency.js is fetched.   
-    //Then your script is executed
-});
