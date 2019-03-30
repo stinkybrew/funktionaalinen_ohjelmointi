d4f84dbf7a0651836d55cc53fa835b9fd47962e1
Wille Tuovinen <wille.tuovinen@metropolia.fi>
2019-03-30 20:53:10 +0200

    update TODO2
    update TODO2

--- w2/01/TODO2.js
+++ w2/01/TODO2.js
@@ -1,30 +1,75 @@
+var moviez = [ 
+		{ 
+ 			title: 'Harry Potter and the Deathly Hallows: Part 2', 
+ 			director: 'David Yates', 
+ 			release: 2011  
+ 		},
+ 		{ 
+ 			title: 'The Lord of the Rings: The Fellowship of the Ring', 
+ 			director: 'Peter Jackson', 
+ 			release: 2001  
+ 		}, 
+ 		{ 
+ 			title: 'Titanic', 
+ 			director: 'James Cameron', 
+ 			release: 1997 
+ 		}, 
+ 		{ 
+ 			title: 'Avatar', 
+ 			director: 'James Cameron', 
+ 			release: 2009 
+ 		}, 
+ 		{ 
+ 			title: 'The Hobbit: An Unexpected Journey', 
+ 			director: 'Peter Jackson', 
+ 			release: 2012 
+ 		}, 
+ 		{ 
+ 			title: 'The Matrix', 
+ 			director: 'Andy Wachowski', 
+ 			release: 1999 
+ 		}, 
+ 		{ 
+ 			title: 'Inception', 
+ 			director: 'Christopher Nolan', 
+ 			release: 2010 
+ 		}, 
+ 		{ 
+ 			title: 'The Town', 
+ 			director: 'Ben Affleck', 
+ 			release: 2010 
+ 		}, 
+ 		{ 
+ 			title: 'The Dark Knight Rises', 
+ 			director: 'Christopher Nolan', 
+ 			release: 2012 
+ 		}, 
+ 		{ 
+ 			title: 'My Neighbor Totoro', 
+ 			director: 'Hayao Miyazaki', 
+ 			release: 1988 
+ 		}, 
+ 		{ 
+ 			title: 'Spirited Away', 
+ 			director: 'Hayao Miyazaki', 
+ 			release: 2001 
+ 		} 
+ 	]	 
+
+	 //11
+
+	var kiku = function(moviez) {
+		return function {
+		var arr1 = [];
+		
+		arr1.push(moviez);
+		return arr1;
+		}
+	};
+	console.log(moviez.map(kiku));
 
 
-
-// pass a function to map
-//const map1 = arr1.map(x => json.parse(x) + "X");
-
-const map1 = function(arr1) {
-    return function() {
-        console.log(arr1);
-    }();   
-}
-var arr1 = ('C:/Users/wille/tmp/testi/funktionaalinen_ohjelmointi/w2/01/movies.js');
-var list=[];
-var tulos = (map1(arr1));
-console.log(map. tulos);
-/*
-const func = function(x, y) {
-    return function () {
-        if (x > y){
-            return (1); // x is warmer that y 
-        }
-        else if (x < y) {
-            return(-1); // x is colder that y
-        }
-        else {
-            return (0); // temps are equal
-        }   
-    }();
-}
-*/
+	/*var total = shoppingCart.map((moviez) => {
+		return prev + curr.qty * curr.price * 0.9;
+	},0);
+	*/
