02bfbfdf3d2abc8599077a3449424a5a6c703992
Wille Tuovinen <wille.tuovinen@metropolia.fi>
2019-04-01 15:06:14 +0300

    aloitus for TODO1
    aloitus for TODO1

---
+++ w2/02/TODO1.js
@@ -0,0 +1,13 @@
+function laskePisteet(pituus, kPiste, lisapisteet){
+
+}
+var hyppypituus = [Math.floor(Math.random() * 140) + 60];
+var kpistevälinorm = 75-99;
+var kpistevälisuuri = 100-130;
+var pistenormperM = 2;
+var pistesuuriperM = 1.8;
+var kpistesaatu=60;
+let pisteet = normaaliLahti(98);
+if () {
+
+}
