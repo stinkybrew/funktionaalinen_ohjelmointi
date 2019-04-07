213fd36a7fc54ba19b6874161861c2f80978450e
stinkybrew <wille.tuovinen@metropolia.fi>
2019-04-07 14:22:23 +0300

    ..
    ...

--- w3/01/src/todo1/Todo1.java
+++ w3/01/src/todo1/Todo1.java
@@ -8,15 +8,18 @@ public class Todo1 {
     public static void main(String[] args) {
 
         Rads raz = new Rads();
-        Converter conv = new Converter();
+        double val = raz.rads();  			// Kutsutaan metodit
+        
+        Converter conv = new Converter();	// Kutsutaan metodit
+        int value = conv.converter(value);
         
         // calls: hihhuu toCelsius = (int fahrenheitz)->(5/9)*(fahrenheit-32); 
-        System.out.println("test print of fahrenheit to celsius: " + conv);
+        System.out.println("test print of fahrenheit to celsius: " + value);
         
         // calls: hihhuuz area = (double radiuz) -> (Math.PI * radius * radius);
         System.out.println("test print of radius calculations: " + raz);
            
-    }        
+    }
     int fahrenheit = 45;
     double rad = 23;
 }
--- w3/01/src/todo2/Todo2.java
+++ w3/01/src/todo2/Todo2.java
@@ -1,10 +1,15 @@
 package todo2;
 
+import streams.*;
+
 // Teht‰v‰nanto
 /* Etsi kaikki rahansiirrot (transaction), 
 jotka ovat tehty vuoden 2012 j‰lkeen ja joiden arvo on v‰hint‰‰n 900 */
 
 public class Todo2 {
-
 	
+	public static void hae(String[] args) {
+		PuttingIntoPractice trans = new PuttingIntoPractice();
+		System.out.println(trans);
+	}
 }
