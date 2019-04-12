5a9abfdc2bb05ff578a8fc4d6df3f48d167e5ccf
Wille Tuovinen <wille.tuovinen@metropolia.fi>
2019-04-12 15:43:09 +0300

    ...
    ...

--- w3/01/src/todo3/Todo3.java
+++ w3/01/src/todo3/Todo3.java
@@ -1,22 +1,43 @@
 package todo3;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 import menu.Dish.Type;
+import streams.Transaction;
+import todo1.Rads;
 
 public class Todo3 {
 
 	public static void main(String[] args) {
 		/* Tee vuo, jossa generoit 20 satunnaislukua, muutat ne nopan silmäluvuiksi sopiviksi välille 1-6,
 		   ja lasket kuinka monta kutosta saadaan.  */
-		 int rand= ((int) (Math.random()* 6)+1);
+		 Integer rand= ((int) (Math.random()* 6)+1);
 		
-		 IntStream.rangeClosed(1, 20)
+	/*	 List<Rads> randomint = Arrays.asList(
+		            new Rads((int) (Math.random()* 6)+1)), 
+		            new Rads(rand),
+		            new Rads(rand),
+		            new Rads(rand),	
+		            new Rads(rand),
+		            new Rads(rand)
+		 );		 //EI TOIMI LISTA TYYLI
+		 
+		 List<Random> randz = randomint.stream()
+		 .map(i -> (randz))
+		 .filter(i -> randz==6)
+		 //.reduce((a,i) -> a + i);
+		 .forEach(System.out::println);  */
+		 
+		 
+		 
+		IntStream.rangeClosed(1, 20)
         .map(i -> (rand))
         .filter(i -> i==6)
 		//.reduce((a,i) -> a + i)
 		.sum();
-		
 		
 		int randomint = randint();
 		System.out.println("Random Number:" + randomint); 
---
+++ w3/01/src/todo4/Todo4.java
@@ -0,0 +1,19 @@
+package todo4;
+
+public class Todo4 {
+
+	public Todo4() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		/* Kahdessa taulukossa on numeroita esim. [1,2,3] ja [3,4]. 
+		   Miten tuotat kaikki numeroparit, 
+		   joissa ensimmäinen numero on ensimmäisestä listasta ja toinen toisesta. 
+		   Esimerkkilistoista tulisi lista, jossa on parit 
+		   [(1,3), (1,4), (2,3), (2,4), (3,3), (3,4)]
+		 */
+
+	}
+
+}
