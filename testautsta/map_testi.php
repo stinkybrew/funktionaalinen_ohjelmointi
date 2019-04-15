7d3b03d4dec04b268ffcaae9775d84ad10e75546
stinkybrew <wille.tuovinen@metropolia.fi>
2019-04-15 23:47:37 +0300

    ...
    ...

---
+++ testautsta/map_testi.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function myfunction($v1,$v2)
+{
+if ($v1===$v2)
+  {
+  return "same";
+  }
+return "different";
+}
+
+$a1=array("Horse","Dog","Cat");
+$a2=array("Cow","Dog","Rat");
+print_r(array_map("myfunction",$a1,$a2));
+
+?>
+
+</body>
+</html>
