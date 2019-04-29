02676be4cf0696146ff33a5b42fcf2725a651148
stinkybrew <wille.tuovinen@metropolia.fi>
2019-04-29 23:55:13 +0300

    --
    ---

--- w5/p02/src/p02/core.clj
+++ w5/p02/src/p02/core.clj
@@ -1,3 +1,14 @@
 (ns p02.core
   (:gen-class))
 
+;Kirjoita ohjelma (funktio), joka kysyy sinulta yhden luvun (> 0) ja kertoo, onko luku parillinen. Jos luku on <= 0, 
+;ohjelma tulostaa virheilmoituksen. Merkkijonosyötteen (read-line) voi muuttaa kokonaisluvuksi operaattorilla Integer., esim. (Integer. "2")
+(println "Anna luku joka on suurempi kuin 0")
+(defn vertailu
+[luku] ; parametrina saatu arvo
+(if (= luku 0) "ERROR! Virheellinen syöte.")
+(if (/ luku 2) "parillinen luku"))
+
+(defn vertailu2
+[luku2] ; parametrina saatu arvo
+(if (<= :0 luku2) "Menen puistoon." "Luennolle."))
