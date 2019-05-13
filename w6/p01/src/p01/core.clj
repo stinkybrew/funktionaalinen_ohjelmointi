8ba98d2f13929dfb8fd45ef349595f4905f46e9a
Wille Tuovinen <wille.tuovinen@metropolia.fi>
2019-05-13 13:15:53 +0300

    w6 01 t1 Done
    w6 01 t1 Done

--- w6/p01/src/p01/core.clj
+++ w6/p01/src/p01/core.clj
@@ -1,3 +1,14 @@
 (ns p01.core
   (:gen-class))
 
+  (def food-journal
+    [{:kk 3 :paiva 1 :neste 5.3 :vesi 2.0}
+     {:kk 3 :paiva 2 :neste 5.1 :vesi 3.0}
+     {:kk 3 :paiva 13 :neste 4.9 :vesi 2.0}
+     {:kk 4 :paiva 5 :neste 5.0 :vesi 2.0}
+     {:kk 4 :paiva 10 :neste 4.2 :vesi 2.5}
+     {:kk 4 :paiva 15 :neste 4.0 :vesi 2.8}
+     {:kk 4 :paiva 29 :neste 3.7 :vesi 2.0}
+     {:kk 4 :paiva 30 :neste 3.7 :vesi 1.0}])
+
+    (def rak (into [] (keep (fn [x] (if (= (get x :kk) 4) (:kk (get x :kk) .paiva (get x :paiva)))))))
