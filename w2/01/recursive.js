89383c79aac295c8fa774510b25e3a769d468170
stinkybrew <wille.tuovinen@metropolia.fi>
2020-03-19 15:01:10 +0200

    prints
    prints

--- w2/01/recursive.js
+++ w2/01/recursive.js
@@ -1,10 +1,12 @@
+var arvo = 4;
 
 function pow(n) {
     if (n == 1) {
-      return 1;
+        console.log(n) // Prints 1
+        return 1;
     } else {
-      return n * pow(n - 1);
+        console.log(n) // Prints all other numbers
+        return n * pow(n - 1);
     }
 }
-
-console.log( pow(4) ); 
+console.log(pow(arvo));
